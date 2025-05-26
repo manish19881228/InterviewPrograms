a8f12fa04105be0d99e7e83a69e8e8f809b2afd3
manish <mk19881228@gmail.com>
2025-05-26 17:14:38 +0530

    added comments

--- src/main/java/com/javatechie/Main.java
+++ src/main/java/com/javatechie/Main.java
@@ -1,8 +1,16 @@
 package com.javatechie;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
-        System.out.println("Hello, mk!");
+        List<Integer> nums= Arrays.asList(1,2,3,4,5);
+
+        List<Integer> reqNums=nums.stream()
+                .filter(n-> n>=3 && n<=4)
+                .collect(Collectors.toList());
+        System.out.println("check:: "+reqNums);
     }
 }
